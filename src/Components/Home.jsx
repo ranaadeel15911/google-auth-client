72fe21213e1914cd5a78ea05ae6c63bc1b576d80
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-04-12 12:10:02 +0500

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,27 +1,11 @@
-import React, { useState } from 'react'
-// import jwt_decode from "jwt-decode";
-import { jwtDecode } from "jwt-decode";
-import { Link } from 'react-router-dom';
+import React from 'react'
 const Home = () => {
-    const token = localStorage.getItem("token")
-    const decoded = jwtDecode(token);
-    // const [profile , setProfile]=useState()
-    console.log(decoded)
-    console.log(decoded.email)
+    
   return (
     <>
     <Link to={'/login'}>Login</Link>
-    <div>{
-        decoded &&
-        <>
-        Emal : {decoded.email}
-        Name : {decoded.name}
-        { decoded.picture && 
-          <img src={decoded.picture} alt="feck" />
-        }
-        </>
-        }
-        
+    <div>
+        home
         </div>
         </>
   )
